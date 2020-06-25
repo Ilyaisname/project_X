ba9674c367ebe5dfc4b86fbbf5b2ba431f9920f0
ilya <microstim@mail.ru>
2020-06-26 00:53:09 +0300

    create all

--- src/components/Process/Process.js
+++ src/components/Process/Process.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Process.css'
+import '../../css/fontello.css'
 
 export default (props) => {
   return(
@@ -14,41 +15,41 @@ export default (props) => {
       </div>
       <div className="Process__wrap">
         <div className="Process__counter">
-          <span className="counter__icon"></span>
-          <span className="counter__number"></span>
+          <span className="_icon"><i className="demo-icon icon-svg_counter"></i></span>
+          <span className="counter__number">340 487</span>
         </div>
-        <div className="Process__time">
+        <div className="Process__time coll">
           <div className="time">
-            <span className="time__icon"></span>
-            <span className="time__time"></span>
+            <span className="_icon"><i className="demo-icon icon-time_time"></i></span>
+            <span className="time__time">10ч 36 мин</span>
           </div> 
           <div className="time">
-            <span className="time__icon"></span>
-            <span className="time__percent"></span>
+            <span className="_icon"><i className="demo-icon icon-sr_time"></i></span>
+            <span className="time__percent">1ч 7 мин (10,5%)</span>
           </div>
         </div>
-        <div className="Process__persons">
+        <div className="Process__persons coll">
           <div className="persons">
-            <span className="persons__icon"></span>
-            <span className="persons__sum"></span>
+            <span className="_icon"><i className="demo-icon icon-people"></i></span>
+            <span className="persons__sum">120 сотрудников</span>
           </div> 
           <div className="persons">
-            <span className="persons__icon"></span>
-            <span className="persons__scripts"></span>
+            <span className="_icon"><i className="demo-icon icon-scripts"></i></span>
+            <span className="persons__scripts">129 сценариев</span>
           </div>
         </div>
-        <div className="Process__date">
+        <div className="Process__date coll">
          <div className="date__wrap">
-            <span className="date__start"></span>
-            <span className="date__date-number"></span>
+            <span className="date__start">Начало</span>
+            <span className="date__date-number">11 ноября 2017</span>
          </div>
          <div className="date__wrap">
-            <span className="date__end"></span>
-            <span className="date__date-number"></span>
+            <span className="date__end">Окончание</span>
+            <span className="date__date-number">6 января 2018</span>
          </div>
          <div className="date__wrap">
-            <span className="date__loading"></span>
-            <span className="date__date-number"></span>
+            <span className="date__loading">Загрузка</span>
+            <span className="date__date-number">10 января 2018</span>
           </div>
         </div>
       </div>
